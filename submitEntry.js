6749729c12402e6e36190211134a9c27bee24c8b
Juliann Internicola <juliann.internicola@gmail.com>
2016-12-02 04:09:41 -0500

    Updaing indes and submitEntry, adding js and css

---
+++ submitEntry.js
@@ -0,0 +1,29 @@
+/*============================
+// Jewish Play Project
+// submitEntry.html Javascript
+============================*/
+
+function change(obj) {
+	var selectBox = obj;
+	var selected = selectBox.options[selectBox.selectedIndex].value;
+	var people = document.getElementById("peopleD");
+	var toys = document.getElementById("toysD");
+	var companies = document.getElementById("companiesD");
+
+	//Hide div depending on user choice.
+	if(selected == 'people'){
+		people.style.display = "block";
+		toys.style.display = "none";
+		companies.style.display = "none";
+	}
+	else if(selected == 'companies'){
+		people.style.display = "none";
+		toys.style.display = "none";
+		companies.style.display = "block";
+	}
+	else{
+		people.style.display = "none";
+		toys.style.display = "block";
+		companies.style.display = "none";
+	}
+}
